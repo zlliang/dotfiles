cbc1681dc0dbb45451e7b8b4fa5deab1b89e8dd0
Zilong Liang <zlliang96@outlook.com>
2020-05-01 17:18:23 +0800

    Update Fish functions

--- fish/functions/create-latex.fish
+++ fish/functions/create-latex.fish
@@ -4,7 +4,7 @@ function create-latex -d "Create a LaTeX document"
   if not test (echo $argv)
     set argv document
   end
-  cp ~/Documents/projects/latex-templates/doc-preamble.tex .
-  cp ~/Documents/projects/latex-templates/document.tex $argv.tex
-  cp ~/Documents/projects/latex-templates/*.bib .
+  cp ~/projects/latex-templates/doc-preamble.tex .
+  cp ~/projects/latex-templates/document.tex $argv.tex
+  cp ~/projects/latex-templates/*.bib .
 end
--- fish/functions/create-slides.fish
+++ fish/functions/create-slides.fish
@@ -4,6 +4,6 @@ function create-slides -d "Create LaTeX slides"
   if not test (echo $argv)
     set argv slides
   end
-  cp ~/Documents/projects/latex-templates/slides-preamble.tex .
-  cp ~/Documents/projects/latex-templates/slides.tex $argv.tex
+  cp ~/projects/latex-templates/slides-preamble.tex .
+  cp ~/projects/latex-templates/slides.tex $argv.tex
 end
