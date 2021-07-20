60015e0d3d87807f5acb8b2d488fe17f66acec4c
Zilong Liang <zlliang96@outlook.com>
2021-07-20 19:23:36 +0800

    Update settings

--- fish/config.fish
+++ fish/config.fish
@@ -9,14 +9,10 @@ set -x HOMEBREW_BOTTLE_DOMAIN https://mirrors.ustc.edu.cn/homebrew-bottles
 # System utilities
 alias ls "exa -lhH --git --time-style='long-iso'"
 alias l "ls"
-alias dust "dust -br"
 
 # HTTP proxy
 # set -x http_proxy  http://127.0.0.1:1087
 # set -x https_proxy http://127.0.0.1:1087
-
-# Node
-set -x PATH /usr/local/opt/node@14/bin $PATH
 
 # Rust
 set -x PATH $HOME/.cargo/bin $PATH
@@ -34,11 +30,4 @@ zoxide init fish | source
 # gitignore.io
 function gi -d "gitignore.io: Create useful .gitignore files"
   curl -sL https://gitignore.io/api/$argv
-end
-
-# git.io
-function gitio -d "git.io: Create short links for GitHub web pages"
-  curl -si https://git.io -F "url=$argv" |
-  grep "Location" |
-  sed "s/Location: //"
 end
