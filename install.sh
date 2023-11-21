12d5704f561a2e06ad4acdb17b9ac13d9c39501e
Zilong Liang <zlliang96@outlook.com>
2023-11-21 11:00:46 +0800

    Update settings

--- install.sh
+++ install.sh
@@ -2,33 +2,37 @@
 # Make hard links of the dotfiles
 
 # Install utilities for remote environments
-if command -q apt-get
+if command -v apt-get; then
   apt-get -y update && apt-get -y install exa ripgrep
-end
+fi
 
-if test ! -d $HOME/.config/fish
+if test ! -d $HOME/.config/fish; then
   mkdir -p $HOME/.config/fish
 fi
-if test ! -d $HOME/.config/fish/functions
+if test ! -d $HOME/.config/fish/functions; then
   mkdir -p $HOME/.config/fish/functions
 fi
 
-ln -vf .gitignore .gitconfig .vimrc .npmrc $HOME
-ln -vf fish/*.fish $HOME/.config/fish
-ln -vf fish/functions/*.fish $HOME/.config/fish/functions
+if test $(uname) = Darwin; then
+  ln -vf .gitignore .gitconfig .vimrc .npmrc $HOME
+  ln -vf fish/*.fish $HOME/.config/fish
+  ln -vf fish/functions/*.fish $HOME/.config/fish/functions
 
-if test (uname) = Darwin
-  if test ! -d $HOME/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
+  if test ! -d $HOME/Library/Application\ Support/iTerm2/Scripts/AutoLaunch; then
     mkdir -p $HOME/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
   fi
-  if test ! -d $HOME/Library/Application\ Support/Code/User
+  if test ! -d $HOME/Library/Application\ Support/Code/User; then
     mkdir -p $HOME/Library/Application\ Support/Code/User
   fi
-  if test ! -d $HOME/Library/Application\ Support/Code/User/snippets
+  if test ! -d $HOME/Library/Application\ Support/Code/User/snippets; then
     mkdir -p $HOME/Library/Application\ Support/Code/User/snippets
   fi
 
   ln -vf iterm/dark-mode.py $HOME/Library/Application\ Support/iTerm2/Scripts/AutoLaunch
   ln -vf vscode/*.json $HOME/Library/Application\ Support/Code/User
   ln -vf vscode/snippets/*.json $HOME/Library/Application\ Support/Code/User/snippets
+else
+  cp .gitignore .gitconfig .vimrc .npmrc $HOME
+  cp fish/*.fish $HOME/.config/fish
+  cp fish/functions/*.fish $HOME/.config/fish/functions
 fi
