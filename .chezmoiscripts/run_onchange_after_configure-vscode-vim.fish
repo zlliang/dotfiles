ae9c5374fb0179e41dcff1b8a655fd1fbe5bd8b6
Zilong Liang <zlliang96@outlook.com>
2026-07-13 10:08:25 +0800

    refactor: organize managed configuration files

--- .chezmoiscripts/run_onchange_after_configure-vscode-vim.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env fish
-
-source "$HOME/.config/fish/config.fish"
-
-# Enable key-repeating for VS Code Vim plugin.
-# See https://marketplace.visualstudio.com/items?itemName=vscodevim.vim.
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
